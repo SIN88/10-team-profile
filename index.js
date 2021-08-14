7b893f026612814d2fc3e1a3c54a182b71e9d37f
SIN88 <KG19882020@GMAIL.COM>
2021-08-14 14:53:52 -0700

    work

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const Manager = require('./Library/Manager');
+const Engineer = require('./Library/Engineer');
+const Intern = require('./Library/Intern');
+const Employee = require('./Library/Employee');
